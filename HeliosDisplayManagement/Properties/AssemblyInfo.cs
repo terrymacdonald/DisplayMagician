6606d98f76492b49b79a6eab904c8b94599b81cc
s_falahati <s_falahati@hotmail.com>
2018-10-20 04:11:57 +0330

    New release

--- HeliosDisplayManagement/Properties/AssemblyInfo.cs
+++ HeliosDisplayManagement/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.9.*")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
