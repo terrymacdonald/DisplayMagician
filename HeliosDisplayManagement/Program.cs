58fa54c995ea45c002b40ab889a8312d10aabe54
s_falahati <s_falahati@hotmail.com>
2018-10-20 03:50:53 +0330

    Fixing a problem with Steam's new TLS policy

--- HeliosDisplayManagement/Program.cs
+++ HeliosDisplayManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -81,6 +82,7 @@ namespace HeliosDisplayManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             try
             {
                 if (!IPCService.StartService())
