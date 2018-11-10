e43ff5e87d44a2622c870224ab66c283c36a1e07
s_falahati <s_falahati@hotmail.com>
2018-11-10 05:05:59 +0330

    Classes required to retrieve and apply Windows TaskBar settings added to the project

---
+++ HeliosDisplayManagement.Shared/ShellHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace HeliosDisplayManagement.Shared
+{
+    public static class ShellHelper
+    {
+        private static readonly uint NotifyMessage_SettingChange = 0x001A;
+        private static readonly uint NotifyMessage_ThemeChanged = 0x031A;
+        private static readonly uint ShellChange_AllEvents = 0x7FFFFFFF;
+        private static readonly uint ShellChange_FlushFlag = 0x1000;
+        private static readonly uint ShellChange_NotifyRecursiveFlag = 0x10000;
+
+        private static readonly UIntPtr WindowHandleBroadcast = (UIntPtr) 0xffff;
+
+        public static Process GetShellProcess()
+        {
+            try
+            {
+                var shellWindowHandle = GetShellWindow();
+
+                if (shellWindowHandle != IntPtr.Zero)
+                {
+                    GetWindowThreadProcessId(shellWindowHandle, out var shellPid);
+
+                    if (shellPid > 0)
+                    {
+                        return Process.GetProcessById((int) shellPid);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return null;
+        }
+
+        public static async Task IntrigueShellToWriteSettings()
+        {
+            try
+            {
+                SendNotifyMessage(WindowHandleBroadcast, NotifyMessage_SettingChange, (UIntPtr) 0, "Policy");
+                SendNotifyMessage(WindowHandleBroadcast, NotifyMessage_ThemeChanged, (UIntPtr) 0, null);
+                ShellChangeNotify(ShellChange_AllEvents, ShellChange_FlushFlag | ShellChange_NotifyRecursiveFlag,
+                    IntPtr.Zero, IntPtr.Zero);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+        }
+
+        [DllImport("user32")]
+        private static extern IntPtr GetShellWindow();
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr windowHandle, out uint processId);
+
+        [DllImport("user32", EntryPoint = "SendNotifyMessage", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool SendNotifyMessage(
+            UIntPtr windowHandle,
+            uint messageId,
+            UIntPtr wParam,
+            string lParam);
+
+        [DllImport("shell32", EntryPoint = "SHChangeNotify", SetLastError = true)]
+        private static extern int ShellChangeNotify(uint eventId, uint flags, IntPtr item1, IntPtr item2);
+    }
+}
---
+++ HeliosDisplayManagement.Shared/TaskBarStuckRectangle.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using WindowsDisplayAPI.DisplayConfig;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace HeliosDisplayManagement.Shared
+{
+    public class TaskBarStuckRectangle
+    {
+        public enum TaskBarEdge : uint
+        {
+            Left = 0,
+            Top = 1,
+            Right = 2,
+            Bottom = 3
+        }
+
+        [Flags]
+        public enum TaskBarOptions : uint
+        {
+            None = 0,
+            AutoHide = 1 << 0,
+            KeepOnTop = 1 << 1,
+            UseSmallIcons = 1 << 2,
+            HideClock = 1 << 3,
+            HideVolume = 1 << 4,
+            HideNetwork = 1 << 5,
+            HidePower = 1 << 6,
+            WindowPreview = 1 << 7,
+            Unknown1 = 1 << 8,
+            Unknown2 = 1 << 9,
+            HideActionCenter = 1 << 10,
+            Unknown3 = 1 << 11,
+            HideLocation = 1 << 12,
+            HideLanguageBar = 1 << 13
+        }
+
+        private const string MainDisplayAddress =
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StuckRects{0:D}";
+
+        private const string MultiDisplayAddress =
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\MMStuckRects{0:D}";
+
+        private static readonly Dictionary<int, byte[]> Headers = new Dictionary<int, byte[]>
+        {
+            {2, new byte[] {0x28, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF}},
+            {3, new byte[] {0x30, 0x00, 0x00, 0x00, 0xFE, 0xFF, 0xFF, 0xFF}}
+        };
+
+        public TaskBarStuckRectangle(int version, string devicePath) : this(version)
+        {
+            DevicePath = devicePath;
+        }
+
+        public TaskBarStuckRectangle(int version)
+        {
+            if (!Headers.ContainsKey(version))
+            {
+                throw new ArgumentException(@"Invalid version number specified.", nameof(version));
+            }
+
+            Version = version;
+            DevicePath = null;
+
+            Binary = new byte[Headers[Version][0]];
+            Array.Copy(Headers[Version], 0, Binary, 0, Headers[Version].Length);
+
+            DPI = 96;
+            Rows = 1;
+            Location = Rectangle.Empty;
+            MinSize = Size.Empty;
+            Edge = TaskBarEdge.Bottom;
+            Options = TaskBarOptions.KeepOnTop;
+        }
+
+        public TaskBarStuckRectangle()
+        {
+        }
+
+        public byte[] Binary { get; set; }
+
+        public string DevicePath { get; set; }
+
+        [JsonIgnore]
+        public uint DPI
+        {
+            get
+            {
+                if (Binary.Length < 44)
+                {
+                    return 0;
+                }
+
+                return BitConverter.ToUInt32(Binary, 40);
+            }
+            set
+            {
+                if (Binary.Length < 44)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes(value);
+                Array.Copy(bytes, 0, Binary, 40, 4);
+            }
+        }
+
+        [JsonIgnore]
+        public TaskBarEdge Edge
+        {
+            get
+            {
+                if (Binary.Length < 16)
+                {
+                    return TaskBarEdge.Bottom;
+                }
+
+                return (TaskBarEdge) BitConverter.ToUInt32(Binary, 12);
+            }
+            set
+            {
+                if (Binary.Length < 16)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes((uint) value);
+                Array.Copy(bytes, 0, Binary, 12, 4);
+            }
+        }
+
+        [JsonIgnore]
+        public Rectangle Location
+        {
+            get
+            {
+                if (Binary.Length < 40)
+                {
+                    return Rectangle.Empty;
+                }
+
+                var left = BitConverter.ToInt32(Binary, 24);
+                var top = BitConverter.ToInt32(Binary, 28);
+                var right = BitConverter.ToInt32(Binary, 32);
+                var bottom = BitConverter.ToInt32(Binary, 36);
+
+                return Rectangle.FromLTRB(left, top, right, bottom);
+            }
+            set
+            {
+                if (Binary.Length < 40)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes(value.Left);
+                Array.Copy(bytes, 0, Binary, 24, 4);
+
+                bytes = BitConverter.GetBytes(value.Top);
+                Array.Copy(bytes, 0, Binary, 28, 4);
+
+                bytes = BitConverter.GetBytes(value.Right);
+                Array.Copy(bytes, 0, Binary, 32, 4);
+
+                bytes = BitConverter.GetBytes(value.Bottom);
+                Array.Copy(bytes, 0, Binary, 36, 4);
+            }
+        }
+
+        [JsonIgnore]
+        public Size MinSize
+        {
+            get
+            {
+                if (Binary.Length < 24)
+                {
+                    return Size.Empty;
+                }
+
+                var width = BitConverter.ToInt32(Binary, 16);
+                var height = BitConverter.ToInt32(Binary, 20);
+
+                return new Size(width, height);
+            }
+            set
+            {
+                if (Binary.Length < 24)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes(value.Width);
+                Array.Copy(bytes, 0, Binary, 16, 4);
+
+                bytes = BitConverter.GetBytes(value.Height);
+                Array.Copy(bytes, 0, Binary, 20, 4);
+            }
+        }
+
+        [JsonIgnore]
+        public TaskBarOptions Options
+        {
+            get
+            {
+                if (Binary.Length < 12)
+                {
+                    return 0;
+                }
+
+                return (TaskBarOptions) BitConverter.ToUInt32(Binary, 8);
+            }
+            set
+            {
+                if (Binary.Length < 12)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes((uint) value);
+                Array.Copy(bytes, 0, Binary, 8, 4);
+            }
+        }
+
+        [JsonIgnore]
+        public uint Rows
+        {
+            get
+            {
+                if (Binary.Length < 48)
+                {
+                    return 1;
+                }
+
+                return BitConverter.ToUInt32(Binary, 44);
+            }
+            set
+            {
+                if (Binary.Length < 48)
+                {
+                    return;
+                }
+
+                var bytes = BitConverter.GetBytes(value);
+                Array.Copy(bytes, 0, Binary, 44, 4);
+            }
+        }
+
+        public int Version { get; set; }
+
+        public static TaskBarStuckRectangle GetCurrent()
+        {
+            return GetCurrent((string) null);
+        }
+
+        public static TaskBarStuckRectangle GetCurrent(PathDisplayTarget pathTargetInfo)
+        {
+            var devicePath = pathTargetInfo?.DevicePath;
+            var index = devicePath?.IndexOf("{", StringComparison.InvariantCultureIgnoreCase);
+
+            if (index > 0)
+            {
+                devicePath = devicePath.Substring(0, index.Value).TrimEnd('#');
+            }
+
+            index = devicePath?.IndexOf("#", StringComparison.InvariantCultureIgnoreCase);
+
+            if (index > 0)
+            {
+                devicePath = devicePath.Substring(index.Value).TrimStart('#');
+            }
+
+            return GetCurrent(devicePath);
+        }
+
+        public static TaskBarStuckRectangle GetCurrent(string devicePath)
+        {
+            var stuckRectanglesVersion = 0;
+            byte[] stuckRectanglesBinary = null;
+
+            // Try to extract the latest version of StuckRectangles available on the User Registry
+            foreach (var version in Headers.Keys)
+            {
+                try
+                {
+                    var address = devicePath != null
+                        ? string.Format(MultiDisplayAddress, version)
+                        : string.Format(MainDisplayAddress, version);
+
+                    using (var key = Registry.CurrentUser.OpenSubKey(
+                        address,
+                        RegistryKeyPermissionCheck.ReadSubTree))
+                    {
+                        var settings = key?.GetValue(devicePath ?? "Settings") as byte[];
+
+                        if (settings?.Length > 0)
+                        {
+                            stuckRectanglesBinary = settings;
+                            stuckRectanglesVersion = version;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+
+            if (stuckRectanglesVersion == 0 || stuckRectanglesBinary == null)
+            {
+                return null;
+            }
+
+            return new TaskBarStuckRectangle
+            {
+                DevicePath = devicePath,
+                Binary = stuckRectanglesBinary,
+                Version = stuckRectanglesVersion
+            };
+        }
+
+        public bool Apply()
+        {
+            if (Binary == null ||
+                Binary.Length == 0 ||
+                Version <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var address = DevicePath != null
+                ? string.Format(MultiDisplayAddress, Version)
+                : string.Format(MainDisplayAddress, Version);
+
+            using (var stuckRectanglesKey = Registry.CurrentUser.OpenSubKey(
+                address,
+                RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                if (stuckRectanglesKey == null)
+                {
+                    return false;
+                }
+
+                try
+                {
+                    stuckRectanglesKey.SetValue(DevicePath ?? "Settings", Binary);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+
+            return true;
+        }
+    }
+}
