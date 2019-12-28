3524845983b892036a15cbf827ceab56d7546e9e
Matthew Mark Strasiotto <matthew.strasiotto@sydney.edu.au>
2019-12-28 23:07:19 +1100

    Update Language

--- HeliosDisplayManagement/Resources/Language.Designer.cs
+++ HeliosDisplayManagement/Resources/Language.Designer.cs
@@ -19,7 +19,7 @@ namespace HeliosDisplayManagement.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Language {
@@ -112,6 +112,24 @@ namespace HeliosDisplayManagement.Resources {
         internal static string Apply_Profile {
             get {
                 return ResourceManager.GetString("Apply_Profile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Applying Paths.
+        /// </summary>
+        internal static string Applying_Paths {
+            get {
+                return ResourceManager.GetString("Applying_Paths", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Applying Topologies.
+        /// </summary>
+        internal static string Applying_Topos {
+            get {
+                return ResourceManager.GetString("Applying_Topos", resourceCulture);
             }
         }
         
