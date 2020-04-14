6bfb3edfba66deee530db46f55f8220d727486f7
temacdonald <terry.macdonald@cosive.com>
2020-04-14 22:20:41 +1200

    [WIP] Added basic Uplay files based on Steam
    This is a basic copy and rename of the Steam files within the app.
    Will be developing the Uplay discovery functionality over the
    following weeks.

---
+++ HeliosDisplayManagement/Uplay/UplayAppIdNamePair.cs
@@ -0,0 +1,18 @@
+﻿namespace HeliosDisplayManagement.Uplay
+{
+    public class UplayAppIdNamePair
+    {
+        public UplayAppIdNamePair(uint appId, string name)
+        {
+            AppId = appId;
+            Name = name;
+        }
+
+        public UplayAppIdNamePair()
+        {
+        }
+
+        public uint AppId { get; set; }
+        public string Name { get; set; }
+    }
+}
