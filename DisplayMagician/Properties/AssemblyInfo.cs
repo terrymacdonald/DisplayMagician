44e7f0df38d300f7442fdde232caf712f06b172d
Terry MacDonald <terry.macdonald@cosive.com>
2021-03-28 20:28:42 +1300

    Update toversion 1.0.2

--- DisplayMagician/Properties/AssemblyInfo.cs
+++ DisplayMagician/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: CLSCompliant(true)]
