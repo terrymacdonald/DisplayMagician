d7d6f46492423041ac3f35198fb87fce0ad15ac9
Terry MacDonald <terry.macdonald@cosive.com>
2021-05-20 17:43:15 +1200

    Setting ApplyProfile top most
    Setting the ApplyProfile to be top most
    again since I fixed the latest profile bug
    and we need it to work properly again.

--- DisplayMagician/UIForms/ApplyingProfileForm.Designer.cs
+++ DisplayMagician/UIForms/ApplyingProfileForm.Designer.cs
@@ -140,6 +140,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DisplayMagician - Please Wait";
+            this.TopMost = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ApplyingProfileForm_FormClosing);
             this.Load += new System.EventHandler(this.ApplyingProfileForm_Reposition);
             this.Shown += new System.EventHandler(this.ApplyingProfileForm_Shown);
