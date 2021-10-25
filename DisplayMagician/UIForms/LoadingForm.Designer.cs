0e0195e3a1f7460c07c36ee2de385e225967622f
Terry MacDonald <terry.macdonald@cosive.com>
2021-10-25 20:45:21 +1300

    Preaparation for a popup window if loading takes too long

---
+++ DisplayMagician/UIForms/LoadingForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace DisplayMagician.UIForms
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingForm));
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.BackColor = System.Drawing.Color.Black;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.ForeColor = System.Drawing.Color.White;
+            this.lbl_title.Location = new System.Drawing.Point(71, 35);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(346, 29);
+            this.lbl_title.TabIndex = 0;
+            this.lbl_title.Text = "DisplayMagician is loading...";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.BackColor = System.Drawing.Color.IndianRed;
+            this.lbl_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_description.ForeColor = System.Drawing.Color.Black;
+            this.lbl_description.Location = new System.Drawing.Point(71, 64);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(346, 28);
+            this.lbl_description.TabIndex = 1;
+            this.lbl_description.Text = "but it\'s taking a while! Please wait.";
+            this.lbl_description.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(495, 129);
+            this.ControlBox = false;
+            this.Controls.Add(this.lbl_description);
+            this.Controls.Add(this.lbl_title);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(511, 168);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(511, 168);
+            this.Name = "LoadingForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "DisplayMagician is loading....";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_description;
+    }
+}
