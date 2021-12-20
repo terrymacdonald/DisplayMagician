c87f55c4a97d6015a6e2812a36a771535c8bd511
Terry MacDonald <terry.macdonald@cosive.com>
2021-12-21 09:30:49 +1300

    Updated version to 2.1.3.0 for release

--- DisplayMagician/Properties/AssemblyInfo.cs
+++ DisplayMagician/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Resources;
 [assembly: Guid("e4ceaf5e-ad01-4695-b179-31168eb74c48")]
 
 // Version information
-[assembly: AssemblyVersion("2.1.2.6")]
-[assembly: AssemblyFileVersion("2.1.2.6")]
+[assembly: AssemblyVersion("2.1.3.1")]
+[assembly: AssemblyFileVersion("2.1.3.1")]
 [assembly: NeutralResourcesLanguageAttribute( "en" )]
 [assembly: CLSCompliant(true)]
 
