4ce83144d64b0668d0967771b752d8a5e87dcd17
Terry MacDonald <terry.macdonald@cosive.com>
2022-06-04 21:11:51 +1200

    Built a testversion

--- DisplayMagician/Properties/AssemblyInfo.cs
+++ DisplayMagician/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Resources;
 [assembly: Guid("e4ceaf5e-ad01-4695-b179-31168eb74c48")]
 
 // Version information
-[assembly: AssemblyVersion("2.4.0.8")]
-[assembly: AssemblyFileVersion("2.4.0.8")]
+[assembly: AssemblyVersion("2.4.0.10")]
+[assembly: AssemblyFileVersion("2.4.0.10")]
 [assembly: NeutralResourcesLanguageAttribute( "en" )]
 [assembly: CLSCompliant(true)]
 
