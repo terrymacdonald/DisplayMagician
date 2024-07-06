5a2210342507b0dbc18bb254c45eb83c5e1e290a
terrymacdonald <terry.macdonald@cosive.com>
2024-07-06 21:12:13 +1200

    Updated AppInfo so that the StringTable works

--- DisplayMagician/GameLibraries/SteamAppInfoParser/AppInfo.cs
+++ DisplayMagician/GameLibraries/SteamAppInfoParser/AppInfo.cs
@@ -11,7 +11,6 @@ using ValveKeyValue;
 
 namespace DisplayMagician.GameLibraries.SteamAppInfoParser
 {
-
     class AppInfo
     {
         private const uint Magic29 = 0x07_56_44_29;
@@ -66,108 +65,6 @@ namespace DisplayMagician.GameLibraries.SteamAppInfoParser
                 reader.BaseStream.Position = offset;
 
                 options.StringTable = stringTable;
-            }
-
-            var deserializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Binary);
-
-            do
-            {
-                var appid = reader.ReadUInt32();
-
-                if (appid == 0)
-                {
-                    break;
-                }
-
-                var size = reader.ReadUInt32(); // size until end of Data
-                var end = reader.BaseStream.Position + size;
-
-                var app = new App
-                {
-                    AppID = appid,
-                    InfoState = reader.ReadUInt32(),
-                    LastUpdated = DateTimeFromUnixTime(reader.ReadUInt32()),
-                    Token = reader.ReadUInt64(),
-                    Hash = new ReadOnlyCollection<byte>(reader.ReadBytes(20)),
-                    ChangeNumber = reader.ReadUInt32(),
-                };
-
-                if (magic == Magic28 || magic == Magic29)
-                {
-                    app.BinaryDataHash = new ReadOnlyCollection<byte>(reader.ReadBytes(20));
-                }
-
-                app.Data = deserializer.Deserialize(input, options);
-
-                if (reader.BaseStream.Position != end)
-                {
-                    throw new InvalidDataException();
-                }
-
-                Apps.Add(app);
-            } while (true);
-        }
-
-        private static DateTime DateTimeFromUnixTime(uint unixTime)
-        {
-            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTime);
-        }
-    }*/
-
-    class AppInfo
-    {
-        private const uint Magic29 = 0x07_56_44_29;
-        private const uint Magic28 = 0x07_56_44_28;
-        private const uint Magic = 0x07_56_44_27;
-
-        public EUniverse Universe { get; set; }
-
-        public List<App> Apps { get; set; } = [];
-
-        /// <summary>
-        /// Opens and reads the given filename.
-        /// </summary>
-        /// <param name="filename">The file to open and read.</param>
-        public void Read(string filename)
-        {
-            using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            Read(fs);
-        }
-
-        /// <summary>
-        /// Reads the given <see cref="Stream"/>.
-        /// </summary>
-        /// <param name="input">The input <see cref="Stream"/> to read from.</param>
-        public void Read(Stream input)
-        {
-            using var reader = new BinaryReader(input);
-            var magic = reader.ReadUInt32();
-
-            if (magic != Magic && magic != Magic28 && magic != Magic29)
-            {
-                throw new InvalidDataException($"Unknown magic header: {magic:X}");
-            }
-
-            Universe = (EUniverse)reader.ReadUInt32();
-
-            var options = new KVSerializerOptions();
-
-            if (magic == Magic29)
-            {
-                var stringTableOffset = reader.ReadInt64();
-                var offset = reader.BaseStream.Position;
-                reader.BaseStream.Position = stringTableOffset;
-                var stringCount = reader.ReadUInt32();
-                var stringPool = new string[stringCount];
-
-                for (var i = 0; i < stringCount; i++)
-                {
-                    stringPool[i] = ReadNullTermUtf8String(reader.BaseStream);
-                }
-
-                reader.BaseStream.Position = offset;
-
-                options.StringPool = stringPool;
             }
 
             var deserializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Binary);
