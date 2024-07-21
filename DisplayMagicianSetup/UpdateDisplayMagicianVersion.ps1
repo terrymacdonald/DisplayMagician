7eccc44acef13d7e6dcf12888fc3abcbe6e2b23a
terrymacdonald <terry.macdonald@cosive.com>
2024-07-21 18:21:24 +1200

    Basic Wix 5.0.1 Package
    This commit works with two basic files within the DisplayMagician and DisplayMagicianConsole projects.

--- DisplayMagicianSetup/UpdateDisplayMagicianVersion.ps1
+++
@@ -1,5 +0,0 @@
- $vdprojPath = "path\to\your\installer.vdproj"
- $newVersion = "1.0.0.0" # Acquire this dynamically from your main project
- $vdprojContent = Get-Content $vdprojPath -Raw
- $vdprojContent = $vdprojContent -replace '"ProductVersion"\s*:\s*"[^"]+"', "`"ProductVersion`": `"$newVersion`""
- Set-Content $vdprojPath -Value $vdprojContent
