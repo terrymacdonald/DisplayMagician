d2ff9e2dc0a148f11df6cf9470df70d34ddcfccd
Terry MacDonald <terry.macdonald@cosive.com>
2026-08-22 13:42:43 +1200

    Updated ProfileSettingsForm layout

--- DisplayMagician/UIForms/ProfileSettingsForm.Designer.cs
+++ DisplayMagician/UIForms/ProfileSettingsForm.Designer.cs
@@ -57,9 +57,10 @@ namespace DisplayMagician.UIForms
             btn_back.FlatAppearance.MouseDownBackColor = System.Drawing.Color.IndianRed;
             btn_back.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Brown;
             btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btn_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             btn_back.ForeColor = System.Drawing.Color.White;
-            btn_back.Location = new System.Drawing.Point(555, 290);
-            btn_back.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btn_back.Location = new System.Drawing.Point(545, 402);
+            btn_back.Margin = new System.Windows.Forms.Padding(55, 19, 55, 19);
             btn_back.Name = "btn_back";
             btn_back.Size = new System.Drawing.Size(88, 27);
             btn_back.TabIndex = 9;
@@ -76,11 +77,11 @@ namespace DisplayMagician.UIForms
             gb_general.Controls.Add(lbl_wallpaper_bg_type);
             gb_general.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             gb_general.ForeColor = System.Drawing.Color.White;
-            gb_general.Location = new System.Drawing.Point(30, 27);
-            gb_general.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            gb_general.Location = new System.Drawing.Point(16, 13);
+            gb_general.Margin = new System.Windows.Forms.Padding(55, 19, 55, 19);
             gb_general.Name = "gb_general";
-            gb_general.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            gb_general.Size = new System.Drawing.Size(612, 108);
+            gb_general.Padding = new System.Windows.Forms.Padding(55, 19, 55, 19);
+            gb_general.Size = new System.Drawing.Size(617, 144);
             gb_general.TabIndex = 11;
             gb_general.TabStop = false;
             gb_general.Text = "Wallpaper Settings";
@@ -90,8 +91,8 @@ namespace DisplayMagician.UIForms
             lbl_wallpaper_mode.AutoSize = true;
             lbl_wallpaper_mode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             lbl_wallpaper_mode.ForeColor = System.Drawing.Color.White;
-            lbl_wallpaper_mode.Location = new System.Drawing.Point(14, 37);
-            lbl_wallpaper_mode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl_wallpaper_mode.Location = new System.Drawing.Point(46, 52);
+            lbl_wallpaper_mode.Margin = new System.Windows.Forms.Padding(55, 0, 55, 0);
             lbl_wallpaper_mode.Name = "lbl_wallpaper_mode";
             lbl_wallpaper_mode.Size = new System.Drawing.Size(179, 16);
             lbl_wallpaper_mode.TabIndex = 0;
@@ -100,13 +101,14 @@ namespace DisplayMagician.UIForms
             // cmb_wallpaper_mode
             // 
             cmb_wallpaper_mode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            cmb_wallpaper_mode.DropDownWidth = 200;
             cmb_wallpaper_mode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             cmb_wallpaper_mode.FormattingEnabled = true;
             cmb_wallpaper_mode.Items.AddRange(new object[] { "Apply saved wallpaper settings", "Do not apply saved wallpaper settings" });
-            cmb_wallpaper_mode.Location = new System.Drawing.Point(228, 34);
-            cmb_wallpaper_mode.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            cmb_wallpaper_mode.Location = new System.Drawing.Point(244, 49);
+            cmb_wallpaper_mode.Margin = new System.Windows.Forms.Padding(55, 19, 55, 19);
             cmb_wallpaper_mode.Name = "cmb_wallpaper_mode";
-            cmb_wallpaper_mode.Size = new System.Drawing.Size(360, 24);
+            cmb_wallpaper_mode.Size = new System.Drawing.Size(200, 24);
             cmb_wallpaper_mode.TabIndex = 1;
             cmb_wallpaper_mode.SelectedIndexChanged += cmb_wallpaper_mode_SelectedIndexChanged;
             // 
@@ -115,8 +117,8 @@ namespace DisplayMagician.UIForms
             lbl_wallpaper_bg_type_label.AutoSize = true;
             lbl_wallpaper_bg_type_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             lbl_wallpaper_bg_type_label.ForeColor = System.Drawing.Color.White;
-            lbl_wallpaper_bg_type_label.Location = new System.Drawing.Point(14, 68);
-            lbl_wallpaper_bg_type_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl_wallpaper_bg_type_label.Location = new System.Drawing.Point(82, 96);
+            lbl_wallpaper_bg_type_label.Margin = new System.Windows.Forms.Padding(55, 0, 55, 0);
             lbl_wallpaper_bg_type_label.Name = "lbl_wallpaper_bg_type_label";
             lbl_wallpaper_bg_type_label.Size = new System.Drawing.Size(145, 16);
             lbl_wallpaper_bg_type_label.TabIndex = 2;
@@ -127,8 +129,8 @@ namespace DisplayMagician.UIForms
             lbl_wallpaper_bg_type.AutoSize = true;
             lbl_wallpaper_bg_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             lbl_wallpaper_bg_type.ForeColor = System.Drawing.Color.LightGray;
-            lbl_wallpaper_bg_type.Location = new System.Drawing.Point(228, 68);
-            lbl_wallpaper_bg_type.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbl_wallpaper_bg_type.Location = new System.Drawing.Point(246, 96);
+            lbl_wallpaper_bg_type.Margin = new System.Windows.Forms.Padding(55, 0, 55, 0);
             lbl_wallpaper_bg_type.Name = "lbl_wallpaper_bg_type";
             lbl_wallpaper_bg_type.Size = new System.Drawing.Size(16, 16);
             lbl_wallpaper_bg_type.TabIndex = 3;
@@ -136,7 +138,7 @@ namespace DisplayMagician.UIForms
             // 
             // gb_multiple_applies
             // 
-            gb_multiple_applies.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            gb_multiple_applies.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             gb_multiple_applies.Controls.Add(label2);
             gb_multiple_applies.Controls.Add(cb_force_restart_explorer);
             gb_multiple_applies.Controls.Add(nud_apply_profile_delay);
@@ -146,9 +148,11 @@ namespace DisplayMagician.UIForms
             gb_multiple_applies.Controls.Add(lbl_seconds);
             gb_multiple_applies.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             gb_multiple_applies.ForeColor = System.Drawing.Color.White;
-            gb_multiple_applies.Location = new System.Drawing.Point(31, 130);
+            gb_multiple_applies.Location = new System.Drawing.Point(16, 169);
+            gb_multiple_applies.Margin = new System.Windows.Forms.Padding(41, 19, 41, 19);
             gb_multiple_applies.Name = "gb_multiple_applies";
-            gb_multiple_applies.Size = new System.Drawing.Size(612, 124);
+            gb_multiple_applies.Padding = new System.Windows.Forms.Padding(41, 19, 41, 19);
+            gb_multiple_applies.Size = new System.Drawing.Size(617, 221);
             gb_multiple_applies.TabIndex = 12;
             gb_multiple_applies.TabStop = false;
             gb_multiple_applies.Text = "Profile Settings";
@@ -157,7 +161,8 @@ namespace DisplayMagician.UIForms
             // 
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            label2.Location = new System.Drawing.Point(52, 94);
+            label2.Location = new System.Drawing.Point(64, 184);
+            label2.Margin = new System.Windows.Forms.Padding(41, 0, 41, 0);
             label2.Name = "label2";
             label2.Size = new System.Drawing.Size(462, 13);
             label2.TabIndex = 6;
@@ -166,7 +171,8 @@ namespace DisplayMagician.UIForms
             // cb_force_restart_explorer
             // 
             cb_force_restart_explorer.AutoSize = true;
-            cb_force_restart_explorer.Location = new System.Drawing.Point(36, 72);
+            cb_force_restart_explorer.Location = new System.Drawing.Point(46, 161);
+            cb_force_restart_explorer.Margin = new System.Windows.Forms.Padding(41, 19, 41, 19);
             cb_force_restart_explorer.Name = "cb_force_restart_explorer";
             cb_force_restart_explorer.Size = new System.Drawing.Size(442, 20);
             cb_force_restart_explorer.TabIndex = 5;
@@ -176,17 +182,19 @@ namespace DisplayMagician.UIForms
             // 
             // nud_apply_profile_delay
             // 
-            nud_apply_profile_delay.Location = new System.Drawing.Point(535, 32);
+            nud_apply_profile_delay.Location = new System.Drawing.Point(204, 103);
+            nud_apply_profile_delay.Margin = new System.Windows.Forms.Padding(562, 122, 562, 122);
             nud_apply_profile_delay.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
             nud_apply_profile_delay.Name = "nud_apply_profile_delay";
-            nud_apply_profile_delay.Size = new System.Drawing.Size(42, 22);
+            nud_apply_profile_delay.Size = new System.Drawing.Size(58, 22);
             nud_apply_profile_delay.TabIndex = 3;
             nud_apply_profile_delay.ValueChanged += nud_apply_profile_delay_ValueChanged;
             // 
             // lbl_apply_profile_delay
             // 
             lbl_apply_profile_delay.AutoSize = true;
-            lbl_apply_profile_delay.Location = new System.Drawing.Point(375, 34);
+            lbl_apply_profile_delay.Location = new System.Drawing.Point(46, 105);
+            lbl_apply_profile_delay.Margin = new System.Windows.Forms.Padding(41, 0, 41, 0);
             lbl_apply_profile_delay.Name = "lbl_apply_profile_delay";
             lbl_apply_profile_delay.Size = new System.Drawing.Size(154, 16);
             lbl_apply_profile_delay.TabIndex = 2;
@@ -194,11 +202,12 @@ namespace DisplayMagician.UIForms
             // 
             // nud_apply_profile_count
             // 
-            nud_apply_profile_count.Location = new System.Drawing.Point(287, 32);
+            nud_apply_profile_count.Location = new System.Drawing.Point(300, 52);
+            nud_apply_profile_count.Margin = new System.Windows.Forms.Padding(562, 122, 562, 122);
             nud_apply_profile_count.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
             nud_apply_profile_count.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             nud_apply_profile_count.Name = "nud_apply_profile_count";
-            nud_apply_profile_count.Size = new System.Drawing.Size(42, 22);
+            nud_apply_profile_count.Size = new System.Drawing.Size(50, 22);
             nud_apply_profile_count.TabIndex = 1;
             nud_apply_profile_count.Value = new decimal(new int[] { 1, 0, 0, 0 });
             nud_apply_profile_count.ValueChanged += nud_apply_profile_count_ValueChanged;
@@ -206,7 +215,8 @@ namespace DisplayMagician.UIForms
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(33, 34);
+            label1.Location = new System.Drawing.Point(46, 54);
+            label1.Margin = new System.Windows.Forms.Padding(41, 0, 41, 0);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(248, 16);
             label1.TabIndex = 0;
@@ -216,7 +226,8 @@ namespace DisplayMagician.UIForms
             // 
             lbl_seconds.AutoSize = true;
             lbl_seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            lbl_seconds.Location = new System.Drawing.Point(526, 53);
+            lbl_seconds.Location = new System.Drawing.Point(262, 106);
+            lbl_seconds.Margin = new System.Windows.Forms.Padding(41, 0, 41, 0);
             lbl_seconds.Name = "lbl_seconds";
             lbl_seconds.Size = new System.Drawing.Size(61, 15);
             lbl_seconds.TabIndex = 4;
@@ -224,16 +235,16 @@ namespace DisplayMagician.UIForms
             // 
             // ProfileSettingsForm
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             BackColor = System.Drawing.Color.Black;
-            ClientSize = new System.Drawing.Size(677, 330);
+            ClientSize = new System.Drawing.Size(650, 443);
             Controls.Add(gb_multiple_applies);
             Controls.Add(gb_general);
             Controls.Add(btn_back);
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
-            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Margin = new System.Windows.Forms.Padding(55, 19, 55, 19);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "ProfileSettingsForm";
