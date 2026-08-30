6a94434a7147ae156ad3368d58f83273e98ed0b0
Terry MacDonald <terry.macdonald@cosive.com>
2026-08-30 20:51:18 +1200

    Remove mono from AudioProfile
    Note: Mono settings can still be controlled via direct API calls, it's just no longer part of the AudioProfile. It was removed due to the fact that Mono Audio settings changes require restarting the audio service, and that requires elevated access via UAC. This was deemed too interrupting to make it part of the AudioProfile process.

--- DisplayMagicianShared/AudioProfileItem.cs
+++ DisplayMagicianShared/AudioProfileItem.cs
@@ -368,12 +368,6 @@ namespace DisplayMagicianShared
             settings += "\tRecording Console Device: " + WindowsAudioConfig.Recording.ConsoleDevice.FriendlyName + Environment.NewLine;
             settings += "\tRecording Volume: " + WindowsAudioConfig.Recording.VolumePercent + Environment.NewLine;
             settings += "\tRecording Mute: " + WindowsAudioConfig.Recording.IsMuted + Environment.NewLine;
-            settings += Environment.NewLine + "System Settings:" + Environment.NewLine;
-            settings += "\tMono Audio: " + (WindowsAudioConfig.System.IsMonoAudioEnabled
-                ? (WindowsAudioConfig.System.MonoAudio ? "On" : "Off")
-                : "Not captured") + Environment.NewLine;
-            settings += "\tSystem Audio Enabled: " + WindowsAudioConfig.System.IsSystemAudioEnabled + Environment.NewLine;
-
             return settings;
         }
 
